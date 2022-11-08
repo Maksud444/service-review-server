217e9af02d532b61495d56767deabda3e629da1e
Maksud444 <83550908+Maksud444@users.noreply.github.com>
2022-11-08 15:30:15 +0600

    aded single food On clicking the view details button, the user will be taken to the allfood  details page:

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -32,6 +32,13 @@ try{
     const allfood = await cursor.toArray();
     res.send(allfood);
  });
+
+ app.get('/allfood/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const service = await foodCollection.findOne(query)
+    res.send(service)
+})
 
 }
 finally{
